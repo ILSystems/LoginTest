f28d28358189704204e0251f2b5a97fd7537f345
Hennik Hunsaker <hennik@insidiouslogic.systems>
2022-11-15 16:58:37 -0700

    Spit screenshots into existing directory

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
+import 'package:integration_test/integration_test_driver.dart'
+    show testOutputsDirectory;
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
   await integrationDriver(
     onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+      final String path = '$testOutputsDirectory/screenshots';
+
+      Directory(path).createSync(recursive: true);
+
       final File image = File(
-        '${Platform.operatingSystem}/screenshots/$screenshotName.png',
+        '$path/$screenshotName.png',
       );
       image.writeAsBytesSync(screenshotBytes);
+
       // Return false if the screenshot is invalid.
       return true;
     },
